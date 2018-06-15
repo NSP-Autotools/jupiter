9371cab6d312c4cdaeb5b76412e28221024d74fe
John Calcote <john.calcote@gmail.com>
2018-06-15 00:10:47 -0600

    Adding a threading library with AC_SEARCH_LIBS.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+static void * print_it(void * data)
+{
+    printf("Hello from %s!\n", (const char *)data);
+    return 0;
+}
 
 int main(int argc, char * argv[])
 {
-    printf("Hello from %s!\n", argv[0]);
+    pthread_t tid;
+    pthread_create(&tid, 0, print_it, argv[0]);
+    pthread_join(tid, 0);
     return 0;
 }
