f3d7b99f7ee36c1c1801a3890e30d74881a37de1
John Calcote <john.calcote@gmail.com>
2018-06-15 18:09:17 -0600

    Converting to Libtool ltdl.

--- src/main.c
+++ src/main.c
@@ -3,8 +3,8 @@
 #include "libjupiter.h"
 #include "module.h"
 
-#if HAVE_DLFCN_H
-# include <dlfcn.h>
+#if HAVE_LTDL_H
+# include <ltdl.h>
 #endif
 
 #define DEFAULT_SALUTATION "Hello"
@@ -14,23 +14,35 @@ int main(int argc, char * argv[])
     int rv;
     const char * salutation = DEFAULT_SALUTATION;
 
-#if HAVE_DLFCN_H
-    void * module;
+#if HAVE_LTDL_H
+    int ltdl;
+    lt_dlhandle module;
     get_salutation_t * get_salutation_fp = 0;
-    module = dlopen("./module.so", RTLD_NOW);
-    if (module != 0)
+
+    LTDL_SET_PRELOADED_SYMBOLS();
+
+    ltdl = lt_dlinit();
+    if (ltdl == 0)
     {
-        get_salutation_fp = (get_salutation_t *)dlsym(module, GET_SALUTATION_SYM);
-        if (get_salutation_fp != 0)
-            salutation = get_salutation_fp();
+        module = lt_dlopen("modules/hithere/hithere.la");
+        if (module != 0)
+        {
+            get_salutation_fp = (get_salutation_t *)lt_dlsym(module, GET_SALUTATION_SYM);
+            if (get_salutation_fp != 0)
+                salutation = get_salutation_fp();
+        }
     }
 #endif
 
     rv = jupiter_print(salutation, argv[0]);
 
-#if HAVE_DLFCN_H
-    if (module != 0)
-        dlclose(module);
+#if HAVE_LTDL_H
+    if (ltdl == 0)
+    {
+        if (module != 0)
+            lt_dlclose(module);
+        lt_dlexit();
+    }
 #endif
 
     return rv;
--- src/modules/hithere/hithere.c
+++ src/modules/hithere/hithere.c
@@ -1,3 +1,4 @@
+#define get_salutation hithere_LTX_get_salutation
 #include "../../module.h"
 
 const char * get_salutation(void)
