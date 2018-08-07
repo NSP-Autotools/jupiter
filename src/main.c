35869db359f9f278049f51981cac63e4809d6d7d
John Calcote <john.calcote@gmail.com>
2018-08-07 11:46:00 -0600

    Adding a convenience library.

--- src/main.c
+++ src/main.c
@@ -1,26 +1,8 @@
 #include "config.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-
-#if HAVE_PTHREAD_H
-# include <pthread.h>
-#endif
-
-static void * print_it(void * data)
-{
-    printf("Hello from %s!\n", (const char *)data);
-    return 0;
-}
+#include "jupcommon.h"
 
 int main(int argc, char * argv[])
 {
-#if ASYNC_EXEC
-    pthread_t tid;
-    pthread_create(&tid, 0, print_it, argv[0]);
-    pthread_join(tid, 0);
-#else
-    print_it(argv[0]);
-#endif
-    return 0;
+    return print_routine(argv[0]);
 }
